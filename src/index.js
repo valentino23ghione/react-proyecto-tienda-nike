14d31644ab490b056cdb83bceb3094af4e252ebc
valentino <valenghionedm@hotmail.com>
2022-08-18 00:32:00 -0300

    desafio actualizacion3

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Prueba from './components/Prueba';
+import Prueba from './Components/Prueba';
 import Navbar from "./Components/Navbar";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
