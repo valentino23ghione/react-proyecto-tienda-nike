fa51510dda6e3659792abc8ff6cd6d1f8218f997
valentino <valenghionedm@hotmail.com>
2022-08-18 01:08:03 -0300

    desafio actualizacion3

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,16 +6,16 @@ return(
 <div>
 <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">Tienda-Nike</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
-        <a class="nav-link" href="#">Features</a>
-        <a class="nav-link" href="#">Pricing</a>
-        <a class="nav-link disabled">Disabled</a>
+        <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+        <a class="nav-link" href="#">Productos</a>
+        <a class="nav-link" href="#">Contacto</a>
+        
       </div>
     </div>
   </div>
