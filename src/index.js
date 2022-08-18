92725925579459fcf09cab09e3a3316547f23944
valentino <valenghionedm@hotmail.com>
2022-08-18 00:13:25 -0300

    actualizacion desafio

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Prueba from './components/Prueba';
-
+import Prueba from "./Components/Navbar";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Prueba/>
+    <App/>
   </React.StrictMode>
 );
 
